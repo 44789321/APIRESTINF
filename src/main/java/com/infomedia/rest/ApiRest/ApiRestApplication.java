25a629ce4364e754d3e0aa96a955998971f3e240
Jaxel562 <erick.juarez@resultant.com>
2025-03-10 15:18:07 -0600

    Add configuration CORS to allowed other conections

--- src/main/java/com/infomedia/rest/ApiRest/ApiRestApplication.java
+++ src/main/java/com/infomedia/rest/ApiRest/ApiRestApplication.java
@@ -3,8 +3,10 @@ package com.infomedia.rest.ApiRest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.infomedia.rest.ApiRest.config")
 public class ApiRestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/infomedia/rest/ApiRest/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.infomedia.rest.ApiRest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Permite estos métodos.
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}

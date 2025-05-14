4970e0c44de4ec61b587847e715d674bf6b8b645
Jaxel562 <erick.juarez@resultant.com>
2025-05-13 18:39:38 -0600

    Cors modified

--- src/main/java/com/infomedia/rest/ApiRest/config/SecurityConfig.java
+++ src/main/java/com/infomedia/rest/ApiRest/config/SecurityConfig.java
@@ -23,8 +23,8 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .cors(Customizer.withDefaults()) // usa configuración de CORS definida abajo
-                .csrf(csrf -> csrf.disable());  // deshabilita CSRF
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
 
         return http.build();
     }
@@ -32,9 +32,16 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));  // En producción, especifica los orígenes permitidos
+        //configuration.setAllowedOrigins(Arrays.asList("*"));  // En producción, especifica los orígenes permitidos
+        configuration.setAllowedOrigins(Arrays.asList("http://3.128.226.54:3200")); //Rutas para el CORS internet
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        configuration.setAllowedHeaders(Arrays.asList(
+                "Authorization",
+                "Content-Type",
+                "X-Requested-With",
+                "x-tkn-ls",
+                "x-tkn-cx"
+        ));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
